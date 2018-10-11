ef100cc80f3a1161984b46b9dccf1ed01716cf46
Tamas Soos <soostamas.hu@gmail.com>
2018-10-11 10:45:46 +0100

    Added localStorage + ability to download notes

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import Markdown from 'react-remarkable'
+import saveAs from 'file-saver';
 
 import Sidebar from './Sidebar';
-// import EditorView from './EditorView';
-// import PresentationView from './PresentationView';
 
 import '../../styles/App.css';
 
@@ -16,38 +15,48 @@ class App extends Component {
       notes: [
         {
           title: 'Untitled',
-          content: 'Write something here...'
+          content: 'Write something here...',
         },
-      ]
+      ],
     };
     this.addNewNote = this.addNewNote.bind(this);
     this.changeNote = this.changeNote.bind(this);
     this.deleteNote = this.deleteNote.bind(this);
     this.handleTitleRename = this.handleTitleRename.bind(this);
     this.handleTextareaChange = this.handleTextareaChange.bind(this);
+    this.downloadNote = this.downloadNote.bind(this);
+  }
+
+  componentWillMount() {
+    localStorage.getItem('notes') && this.setState({
+      currentlyActive: 0,
+      notes: JSON.parse(localStorage.getItem('notes')),
+    });
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem('notes', JSON.stringify(nextState.notes));
   }
 
   addNewNote() {
     this.setState({
-      ...this.state,
       notes: [...this.state.notes, {title: 'Untitled', content: 'Write something here...'}]
     });
   }
 
   changeNote(e) {
-    this.setState({...this.state, currentlyActive: e.target.id});
+    this.setState({currentlyActive: e.target.id});
   }
 
   deleteNote(e) {
     e.stopPropagation();
 
-    const deleteID = e.target.parentNode.id;
+    const deleteID = e.target.getAttribute('data-key');
     const newStateNotes = [...this.state.notes];
     newStateNotes.splice(deleteID, 1);
 
     this.setState({
-      ...this.state,
-      currentlyActive: deleteID - 1,
+      currentlyActive: 0,
       notes: newStateNotes
     });
   }
@@ -56,20 +65,29 @@ class App extends Component {
     const newStateNotes = [...this.state.notes];
     newStateNotes[this.state.currentlyActive].title = e.target.value;
 
-    this.setState({...this.state, notes: newStateNotes });
+    this.setState({notes: newStateNotes });
   }
 
   handleTextareaChange(e){
     const newStateNotes = [...this.state.notes];
     newStateNotes[this.state.currentlyActive].content = e.target.value;
 
-    this.setState({...this.state, notes: newStateNotes });
+    this.setState({notes: newStateNotes });
 
     e.target.style.height = "5px";
     e.target.style.height = (e.target.scrollHeight)+"px";
   }
 
+  downloadNote(e) {
+    const targetNote = this.state.notes[e.target.getAttribute('data-key')];
+    const data = new Blob([targetNote.content], {type: 'text/markdown'});
+    saveAs(data, `${targetNote.title}.md`);
+  }
+
   render() {
+
+    const {currentlyActive, notes} = this.state;
+
     return (
       <div>
 
@@ -77,7 +95,7 @@ class App extends Component {
           addNewNote={this.addNewNote}
           changeNote={this.changeNote}
           deleteNote={this.deleteNote}
-          notes={this.state.notes}/>
+          notes={notes}/>
 
         <div className="container">
           <div className="row">
@@ -85,21 +103,23 @@ class App extends Component {
             <div className="col-12">
               <h2><input
                 className='title-input'
-                value={this.state.notes[this.state.currentlyActive].title}
-                onChange={this.handleTitleRename}
-                ></input></h2>
+                value={notes[currentlyActive].title}
+                onChange={this.handleTitleRename}></input></h2>
+              <button
+                onClick={this.downloadNote}
+                data-key={currentlyActive}>download 2</button>
             </div>
 
             <div className="col-5 editor-view">
               <textarea
                 className="markdown-editor"
-                value={this.state.notes[this.state.currentlyActive].content}
+                value={notes[currentlyActive].content}
                 onChange={this.handleTextareaChange}>
               </textarea>
             </div>
 
             <div className="col-5 presentation-view">
-              <Markdown source={this.state.notes[this.state.currentlyActive].content} />
+              <Markdown source={notes[currentlyActive].content} />
             </div>
 
           </div>
--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Sidebar extends Component {
-  render() {
-    return (
-      <div className="side-bar">
-        <h2>Notes</h2>
-        <div className="saved-notes">
-          { this.props.notes.map((e, i) =>
-            <div id={i} className='note' onClick={this.props.changeNote}>
-              {e.title}
-              <span className='delete-note' onClick={this.props.deleteNote}>x</span>
-            </div>
-          ) }
-          <button className="add-note" onClick={this.props.addNewNote}>+Add new</button>
-        </div>
+const Sidebar = (props) => {
+  return (
+    <div className="side-bar">
+      <h2>Notes</h2>
+      <div className="saved-notes">
+
+        { props.notes.map((e, i) =>
+          <div id={i} key={i} className='note' onClick={props.changeNote}>
+            {e.title}
+            <span className='delete-note' data-key={i} onClick={props.deleteNote}>x</span>
+          </div>
+        ) }
+
+        <button className="add-note" onClick={props.addNewNote}>+Add new</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Sidebar;
